a19e47f6f5211273ec7c78e15b0d16dbc4f2b24a
Alok <alok@alok>
2019-04-29 15:38:56 +0530

    adding custom exception

---
+++ restfull-web-services/src/main/java/com/alok/restfullwebservices/CustomExceptionHandler.java
@@ -0,0 +1,26 @@
+package com.alok.restfullwebservices;
+
+import java.util.Date;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.context.request.WebRequest;
+import org.springframework.web.servlet.mvc.method.annotation.ResponseEntityExceptionHandler;
+
+@ControllerAdvice
+@RestController
+public class CustomExceptionHandler extends ResponseEntityExceptionHandler {
+	
+	@ExceptionHandler(Exception.class)
+	public final ResponseEntity<Object> handleAllException(Exception ex, WebRequest request) 
+	{
+		ExceptionResponse exceptionResponse = new ExceptionResponse(new Date(), ex.getMessage(), request.getDescription(false));
+		
+		return new ResponseEntity<Object>(exceptionResponse, HttpStatus.INTERNAL_SERVER_ERROR);
+	}
+	
+
+}
