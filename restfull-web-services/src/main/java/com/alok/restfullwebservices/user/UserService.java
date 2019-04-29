28f7af7b0fbd11f71b3447f675542eaf5d1c058a
Alok <Alok@Alok>
2019-04-29 14:04:01 +0530

    changing response

--- restfull-web-services/src/main/java/com/alok/restfullwebservices/user/UserService.java
+++ restfull-web-services/src/main/java/com/alok/restfullwebservices/user/UserService.java
@@ -1,13 +1,16 @@
 package com.alok.restfullwebservices.user;
 
+import java.net.URI;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
 @RestController
 public class UserService {
@@ -28,9 +31,17 @@ public class UserService {
 	}
 	
 	@PostMapping("/users")
-	public void setUser(@RequestBody User user)
+	public ResponseEntity<Object> setUser(@RequestBody User user)
 	{
 		User saved = service.save(user);
+		
+		// /user/{id} 
+		
+		URI uri = ServletUriComponentsBuilder.fromCurrentRequest().path("/{id}").buildAndExpand(saved.getId()).toUri();
+		
+		return ResponseEntity.created(uri).build();
+		
+		
 	}
 
 }
