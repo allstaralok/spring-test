b6ae68f953f4e4e2df1e7b65fb114e94623419c4
Alok <Alok@Alok>
2019-04-29 14:13:39 +0530

    adding error 404

---
+++ restfull-web-services/src/main/java/com/alok/restfullwebservices/user/UserNotFoundException.java
@@ -0,0 +1,12 @@
+package com.alok.restfullwebservices.user;
+
+public class UserNotFoundException extends RuntimeException {
+
+	public UserNotFoundException(String message) {
+		super(message);
+		// TODO Auto-generated constructor stub
+	}
+	
+	
+
+}
--- restfull-web-services/src/main/java/com/alok/restfullwebservices/user/UserService.java
+++ restfull-web-services/src/main/java/com/alok/restfullwebservices/user/UserService.java
@@ -27,7 +27,11 @@ public class UserService {
 	@GetMapping("/users/{id}")
 	public User retriveOne(@PathVariable int id)
 	{
-		return service.findOne(id);
+		User user =  service.findOne(id);
+		if(user == null )
+			throw new UserNotFoundException("id - " + id);
+		
+		return user;
 	}
 	
 	@PostMapping("/users")
